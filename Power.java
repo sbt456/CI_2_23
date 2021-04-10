151d9290a93c3f20931424765d4c38c3b7f785a0
sbt456 <77896705+sbt456@users.noreply.github.com>
2021-04-10 13:54:09 -0400

    First update

--- Power.java
+++ Power.java
@@ -1,5 +1,6 @@
 
 public class Power {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,7 +23,21 @@ public class Power {
 		System.out.println(toPower[0][1]*toPower[0][1]);
 		System.out.println(toPower[0][0]*toPower[0][0]);
 		
+		int a = 0;
+		int b = 1;
+		int c = 2;
+		int d = 3;
+		int e = 2;
 		
+		System.out.println("+++++++++++++++");
+		
+		System.out.println(Math.pow(a, e));
+		System.out.println(Math.pow(b, e));
+		System.out.println(Math.pow(c, e));
+		System.out.println(Math.pow(d, e));
+				
+
+				
 	}
 
 }
