d5a0614527eb75ca5e9f2cd4f28466db399caad0
sbt456 <77896705+sbt456@users.noreply.github.com>
2021-04-10 00:53:39 -0400

    Hashmap (Cars)

--- Main.java
+++ Main.java
@@ -1,10 +1,33 @@
 import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 
+		Scanner scanner = new Scanner (System.in);
+
+	    System.out.println("Welcome to High Interest Rate Honda! ");
+	    
+	    // String input
+	  
+	    System.out.println("Would you like to purchase a Honda? (yes or no)");
+	    
+	    String answer = scanner.nextLine();
+	    
+	    if(answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes")){
+	     
+	    	System.out.println("Hondas are nice vehicles ");
+	 
+	    }else {
+	    	System.out.println("Currently we only have Hondas in stock.");
+	    	
+		
+	    }
+		
+		
 		    HashMap<String, String> cars = new HashMap<String, String>();
 		    cars.put("Civic", "Honda");
 		    cars.put("Accord", "Honda");
