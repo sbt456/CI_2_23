46d579391f7d24d96ad24cd9e6aac377574b6bc0
sbt456 <77896705+sbt456@users.noreply.github.com>
2021-04-10 03:40:34 -0400

    Multi Dimensional Array

---
+++ Power.java
@@ -0,0 +1,28 @@
+
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] size = {0,1,2,3};
+		
+		int [][] toPower = {
+				{0,1,2,3},
+				{2}
+		};
+				
+		System.out.println(toPower[0][0]);
+		System.out.println(toPower[0][1]);
+		System.out.println(toPower[0][2]);
+		System.out.println(toPower[0][3]);
+		
+		
+		System.out.println(toPower[0][3]*toPower[0][3]);
+		System.out.println(toPower[0][2]*toPower[0][2]);
+		System.out.println(toPower[0][1]*toPower[0][1]);
+		System.out.println(toPower[0][0]*toPower[0][0]);
+		
+		
+	}
+
+}
