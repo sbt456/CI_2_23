5359d86a2751faf7c04adc6ce52313e5b47a2ce2
sbt456 <77896705+sbt456@users.noreply.github.com>
2021-04-10 16:26:17 -0400

    week 7

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 5;
+		int num2 = 3;
+		int num3 = 2;
+		int num4 = 3;
+		double num5 = 8;
+				
+				
+				
+		int a = num1 + num2;
+		int b = num1 - num2;
+		int c = num1 * num2;
+		int d = num4 / num2;
+		
+		
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num4 / num2);
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		System.out.println(Math.sqrt(num5));
+		
+		
+		
+				
+		
+		
+		
+		
+	
+	}
+
+}
---
+++ MagicCalculator.java
@@ -0,0 +1,38 @@
+
+public class MagicCalculator extends Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = 5;
+
+		System.out.println(Math.sqrt(x));
+		System.out.println(Math.signum(x));
+		System.out.println(Math.cos(x));
+		System.out.println(Math.tan(x));
+		
+		System.out.println("Random -> " + Math.random());
+		
+		
+		System.out.println();
+		
+		int i,fact=1;  
+		  int number=5;    
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		
+		
+
+		
+		
+		
+	
+
+	
+	
+	//System.out.println("The factorial of " + x + " is " + factorial(x) + " . ");
+	}
+	}
+
